19364bbf6f5a1ca0fbc2942a72ace5c4c513d27c
deniskhud <tessladen@gmail.com>
2025-02-23 14:24:16 +0100

    Add files via upload

---
+++ isPalinrome.cpp
@@ -0,0 +1,28 @@
+﻿#include <string>
+#include <iostream>
+using namespace std;
+bool isPalindrome(string s) {
+    string str;
+    for (char x : s) {
+        if (isalnum(x)) {
+            str += tolower(x);
+        }
+    }
+    string copy = str;
+    reverse(str.begin(), str.end());
+    return str == copy;
+}
+int main()
+{
+    cout << "enter the string to check for a palindrome" << endl;
+    string str;
+    cin >> str;
+    if (isPalindrome(str)) {
+        cout << "the string is a palindrome" << endl;
+    }
+    else {
+        cout << "the string is not a palindrome" << endl;
+    }
+
+}
+
